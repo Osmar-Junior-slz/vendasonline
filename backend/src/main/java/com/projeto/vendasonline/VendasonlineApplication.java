9a77143f260df54bad9e5f7c57d1ed0ed95174e7
Osmar-Junior-slz <osmarjuniorslz@gmail.com>
2021-05-09 15:10:22 -0300

    Projetos Criados

---
+++ backend/src/main/java/com/projeto/vendasonline/VendasonlineApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.vendasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasonlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/vendasonline/VendasonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.vendasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
